e2f78bf1293acecdfbcc0a1c3303cfceff371e97
apally <adrien.pally@orange.com>
2025-03-06 10:13:13 +0100

    fix: depracated Registrar import

--- android/src/main/kotlin/me/akoraingdkb/app_launcher/AppLauncherPlugin.kt
+++ android/src/main/kotlin/me/akoraingdkb/app_launcher/AppLauncherPlugin.kt
@@ -12,7 +12,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** AppLauncherPlugin */
 class AppLauncherPlugin : FlutterPlugin, MethodCallHandler {
